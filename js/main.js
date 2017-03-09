8e1ec54d5f680ef4d97c8aec65d2045d9970c02f
kyourselfer <kyourselfer@gmail.com>
2017-03-09 23:40:28 +0300

    Set theme jekyll-theme-hacker

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-//////// «адание 1 ////////
+//////// Задание 1 ////////
 console.log('\n');
-console.log('//////// «адание 1 ////////');
+console.log('//////// Задание 1 ////////');
 /*
-ѕри щелчке мышью на любую клетку доски Ц необходимо писать ее адрес в произвольное место страницы, например, в div. јдрес должен извлекатьс¤ в Ђшахматномї формате. Ќапример: A1, G6 и тп. ѕри этом, ¤чейка, на которую нажали должна помечатьс¤ произвольным образом, например, выделением рамки или другим цветом. ѕри выделении другой ¤чейки, предыдуща¤ должна возвращатьс¤ к первоначальному виду.
-Ќаучитьс¤ обрабатывать стрелки клавиатуры таким образом, чтобы активную ¤чейку из предыдущего пункта можно было перемещать по доске. ≈сли ¤чейка выходит за границы таблицы Ц она должна по¤витьс¤ с другой стороны. ѕри перемещении ¤чейки, так же должен извлекатьс¤ ее адрес.
+При щелчке мышью на любую клетку доски – необходимо писать ее адрес в произвольное место страницы, например, в div. Адрес должен извлекаться в «шахматном» формате. Например: A1, G6 и тп. При этом, ячейка, на которую нажали должна помечаться произвольным образом, например, выделением рамки или другим цветом. При выделении другой ячейки, предыдущая должна возвращаться к первоначальному виду.
+Научиться обрабатывать стрелки клавиатуры таким образом, чтобы активную ячейку из предыдущего пункта можно было перемещать по доске. Если ячейка выходит за границы таблицы – она должна появиться с другой стороны. При перемещении ячейки, так же должен извлекаться ее адрес.
 */
-// создаЄм доску
+// создаём доску
 window.stack = [];
 
 function bildCell() {
@@ -16,33 +16,33 @@ function bildCell() {
 		rn.innerHTML = " ";
 		table.appendChild(rn);
 		var j=1;
-		while(j!==10) { // ¤чейка
+		while(j!==10) { // ячейка
 			var cn = crD();
 			cn.className = "cell";
 			cn.innerHTML = " ";
-			// «аполнение строк rn ¤чейками cn 
+			// Заполнение строк rn ячейками cn 
 			if(i==1 && j>=1) { cn.innerHTML = arr[j-1]; }
 			if(i>=2 && j==1) { cn.innerHTML = arrN[i-1]; }
-			// Ѕелые фигуры
+			// Белые фигуры
 			if(i==8 && j>=2) { cn.innerHTML = arrSw[0]; }
 			if(i==9 && j>=2) { cn.innerHTML = arrSw[j-1]; }
-			// „ерные фигуры
+			// Черные фигуры
 			if(i==3 && j>=2) { cn.innerHTML = arrSb[0]; }
 			if(i==2 && j>=2) { 
 				cn.innerHTML = arrSb[j-1];
-				// ћен¤ем пор¤док черных фигур у корол¤ и королевы
+				// Меняем порядок черных фигур у короля и королевы
 				if(j==5) { cn.innerHTML = arrSb[j];}
 				if(j==6) { cn.innerHTML = arrSb[j-2];
 			}
 		}
-		//  расим нечетные ¤чейки cn 
+		// Красим нечетные ячейки cn 
 		rn.appendChild(cn);
 		if((j%2 !== 0 && i%2 !== 0) || (j%2 == 0 && i%2 == 0)) { bgColor(i,j,'black'); }
 		j++;
 	}
 	i++;	
 }
-//  расим всю 1-ю строку и 1-й столбец
+// Красим всю 1-ю строку и 1-й столбец
 var k=9;
 while(k!==0) {
 	var h=k;
@@ -54,18 +54,18 @@ while(k!==0) {
 	k--;
 }
 }
-// ¬ывод координат клика на доске
+// Вывод координат клика на доске
 function bildDisplay(pY,pX) {
 	var rn1 = crD(), display = crD();
 	display.className = "display";
-	display.innerHTML = "»нформаци¤";
+	display.innerHTML = "Информация";
 	document.body.appendChild(display);
 	rn1.className = "displayIn";
 	rn1.innerHTML = pY + '' + pX;
 	display.appendChild(rn1);
 }
 
-// красим ¤чейки
+// красим ячейки
 function bgColor(arg1,arg2,arg3) {
 	return el.childNodes[1].childNodes[arg1].childNodes[arg2].style.background=arg3;
 }
@@ -73,10 +73,10 @@ function bgColorRandom(min,max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// —оздать элемент
+// Создать элемент
 function crD() { return document.createElement('div'); }
 
-//////////////////  одим //////////////////
+////////////////// Кодим //////////////////
 
 var table = crD(), el = document.getElementById('chess'); // 
 
@@ -84,22 +84,22 @@ table.className = "table";
 table.innerHTML = "<strong>Chess board - JS</strong>";
 el.appendChild(table);
 
-// ћассивы нумерации + фигуры
+// Массивы нумерации + фигуры
 var arr = [' ','A','B','C','D','E','F','G','H'],
 arrN = [' ','1' ,'2' ,'3' ,'4' ,'5' ,'6' ,'7' ,'8' ],
 arrSw = ['&#9817;','&#9814;', '&#9816;', '&#9815;', '&#9813;', '&#9812;', '&#9815;', '&#9816;', '&#9814;'],
 arrSb = ['&#9823;','&#9820;', '&#9822;', '&#9821;', '&#9819;', '&#9818;', '&#9821;', '&#9822;', '&#9820;'];
 
-// ѕостроить доску с фигурами
+// Построить доску с фигурами
 bildCell();
-var ir1='Ќажмите на любую ¤чейку',
+var ir1='Нажмите на любую ячейку',
     ir0='';
 bildDisplay(ir1,ir0);
-// Ћовим клик мышью
+// Ловим клик мышью
 el.onclick=function(event) {
-	// ¬ыдел¤ем номер строки клика
+	// Выделяем номер строки клика
 	ir0 = event.path[1].firstChild.nextElementSibling.innerHTML;
-	// ѕодсчитаем кол-во соседий слево от клика
+	// Подсчитаем кол-во соседий слево от клика
 	function countSib(event) {
 		var els = event.path[0].previousElementSibling, counter=0;
 		while(els !== null) {
@@ -115,17 +115,17 @@ el.onclick=function(event) {
 			ir1 = arr[i+1];
 			i++;
 		}
-		//  расим фон ¤чейки
+		// Красим фон ячейки
 		function bgColor(arg1,arg2,arg3) {
 			return el.childNodes[1].childNodes[arg1].childNodes[arg2].style.background=arg3;
 		}
-		// ћен¤ем цвет фона ¤чейки на произвольный
+		// Меняем цвет фона ячейки на произвольный
 		function bgColorRandom(min,max) {
 			return Math.floor(Math.random() * (max - min + 1)) + min;
 		}
 		
 		bgColor(pY,pX,'#' + bgColorRandom(0,255));
-		// «апоминаем последнюю ¤чейку клика
+		// Запоминаем последнюю ячейку клика
 		pYpX = [pY,pX];
 		
 
@@ -135,7 +135,7 @@ el.onclick=function(event) {
 		ir1 = 'Click to the Chess board, please';
 		ir0 = '';
 	}
-	// ѕерестроим Display дл¤ отображени¤ информации с данными из event
+	// Перестроим Display для отображения информации с данными из event
 	bildDisplay(ir1,ir0);
 
 	
@@ -147,7 +147,7 @@ function adder() {
 		return el.childNodes[1].childNodes[arg1].childNodes[arg2].style.background=arg3;
 	}
 	function detectSiblingColor(arg1,arg2,arg3) {
-// получаем заначение цвета фона справа от кликнутой ¤чейки
+// получаем заначение цвета фона справа от кликнутой ячейки
 		var rig = el.childNodes[1].childNodes[arg1].childNodes[arg2].nextElementSibling.style.cssText;
 		console.log('Y & X: ', arg1, ' ', arg2);
 		console.log('previousEl: ', el.childNodes[1].childNodes[arg1].childNodes[arg2].previousElementSibling.style.cssText);
@@ -172,7 +172,7 @@ function adder() {
 	else if(stack.length >= 2) { // 2-st click
 //		console.log('2-step',stack, pYpX,'\nstack[0]',stack[0]);		
 
-		//  расим ¤чейку обратно W or B
+		// Красим ячейку обратно W or B
 		detectSiblingColor(stYX[0],stYX[1],'red'); //////////////////////////////////////// откорректировать при помощи alert'ов
 ////////////		bgColor(stYX[0],stYX[1],'red');
 
@@ -180,7 +180,7 @@ function adder() {
 //		console.log('4-step',arrp);
 	}
 //// изучить с практикой addEventListener!
-/// ќ„≈–≈ƒ№ -- http://shuvalov.info/2013/03/21/stack-and-queue/
+/// ОЧЕРЕДЬ -- http://shuvalov.info/2013/03/21/stack-and-queue/
 //	}
 	else {
 		detectSiblingColor(stYX[0],stYX[1],'red'); //////////////////////////////////////// откорректировать при помощи alert'ов
@@ -208,7 +208,7 @@ for(i; i<=1; i++) {
 }
 console.log(pYpXlast);*/
 
-//  ѕри выделении другой ¤чейки, предыдуща¤ должна возвращатьс¤ к первоначальному виду.
+//  При выделении другой ячейки, предыдущая должна возвращаться к первоначальному виду.
 
 
 
